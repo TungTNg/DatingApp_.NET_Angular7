807bef69d103550586494ba9473df1dd0e461d25
Tung Nguyen <39467840+TungTNg@users.noreply.github.com>
2019-02-16 20:33:36 -0800

    Created basuc Angular template forms

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -10,11 +11,12 @@ import { NavComponent } from './nav/nav.component';
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
