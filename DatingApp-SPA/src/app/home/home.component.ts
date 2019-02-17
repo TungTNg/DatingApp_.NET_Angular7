a2948c557572a34a8d73d4762b42730455c4c784
Tung Nguyen <39467840+TungTNg@users.noreply.github.com>
2019-02-17 02:33:04 -0800

    Added the register method to the Auth Service

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -9,24 +8,14 @@ import { environment } from '../../environments/environment';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get(environment.API_URL + 'api/values').subscribe(response => {
-        this.values = response;
-    }, error => {
-        console.log(error);
-    });
   }
 
   cancelRegisterMode(registerMode: boolean) {
